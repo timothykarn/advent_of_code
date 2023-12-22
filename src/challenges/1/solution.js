637bbc44d93afb711b217270f7ad463beb063c09
timothykarn <timtimkarn@gmail.com>
2023-12-22 12:48:03 -0800

    fixed data input.json

--- src/challenges/1/solution.js
+++ src/challenges/1/solution.js
@@ -23,8 +23,6 @@ for (const entry of input) {
     if (!/\d/.test(num1)) num1 = castToNumber[num1];
     if (!/\d/.test(num2)) num2 = castToNumber[num2];
     if (!num2) num2 = num1;
-    //if (!doubleNumberRegex.test(entry)) console.log(entry)
-    //console.log(entry, num1, num2)
     total += parseInt(num1 + num2);
 }
 
